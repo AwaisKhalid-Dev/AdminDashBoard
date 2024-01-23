e10cd04ce64d7c72c8a8e6b0448c4e0de51308c7
DESKTOP-TQL4AMR\Awais Khalid <dev.awaiskhalid@gmail.com>
2024-01-23 15:41:02 +0500

    add barchart

---
+++ src/Components/Graphs/LineGraph/chartConfig.js
@@ -0,0 +1,37 @@
+// chartConfig.js
+
+const posList = [
+     'left',
+  'right',
+  'top',
+  'bottom',
+  'inside',
+  'insideTop',
+  'insideLeft',
+  'insideRight',
+  'insideBottom',
+  'insideTopLeft',
+  'insideTopRight',
+  'insideBottomLeft',
+  'insideBottomRight'
+  ];
+  
+  const app = {
+    configParameters: {
+      // Your config parameters
+    },
+    config: {
+      // Your config
+    }
+  };
+  
+  const labelOption = {
+    // Your label option
+  };
+  
+  const option = {
+    // Your chart option
+  };
+  
+  export default option;
+  
--- src/Components/Graphs/LineGraph/index.js
+++ src/Components/Graphs/LineGraph/index.js
@@ -1,32 +1,124 @@
-import React from 'react'
-import ReactEcharts from 'echarts-for-react';
-import Data from '../../../assets/DataJsons/LineGraphData.json';
-export default function index() {
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
 
-    const graph1 = {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+const EChartsComponent = () => {
+  const chartRef = useRef(null); // This ref will point to the div where the chart will be rendered
+  const myChart = useRef(null); // This ref will store the chart instance
+
+  // The chart configuration
+  const getOption = () => ({
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: { type: 'shadow' }
+    },
+    legend: {
+      data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+    },
+    toolbox: {
+      show: true,
+      orient: 'vertical',
+      left: 'right',
+      top: 'center',
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ['line', 'bar', 'stack'] },
+        restore: { show: true },
+        saveAsImage: { show: true }
+      }
+    },
+    xAxis: [{
+      type: 'category',
+      axisTick: { show: false },
+      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May']
+    }],
+    yAxis: [{
+      type: 'value'
+    }],
+    series: [
+      {
+        name: 'Zone1',
+        type: 'bar',
+        barGap: 0,
+        label: {
+          show: true,
+          position: 'insideBottom',
+          distance: 15,
+          align: 'left',
+          verticalAlign: 'middle',
+          rotate: 90,
+          formatter: '{c}  {name|{a}}',
+          fontSize: 16,
+          rich: { name: {} }
         },
-        yAxis: {
-          type: 'value'
+        emphasis: { focus: 'series' },
+        data: [150, 200, 301, 334, 390]
+      },
+      {
+        name: 'Zone2',
+        type: 'bar',
+        barGap: 0,
+        label: {
+          show: true,
+          position: 'insideBottom',
+          distance: 15,
+          align: 'left',
+          verticalAlign: 'middle',
+          rotate: 90,
+          formatter: '{c}  {name|{a}}',
+          fontSize: 16,
+          rich: { name: {} }
         },
-        series: [{
-          data: [80, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line',
-          smooth: true
-        }]
-      };
+        emphasis: { focus: 'series' },
+        data: [300, 300, 180, 130, 270]
+      },
+
+      {
+        name: 'Zone3',
+        type: 'bar',
+        barGap: 0,
+        label: {
+          show: true,
+          position: 'insideBottom',
+          distance: 15,
+          align: 'left',
+          verticalAlign: 'middle',
+          rotate: 90,
+          formatter: '{c}  {name|{a}}',
+          fontSize: 16,
+          rich: { name: {} }
+        },
+        emphasis: { focus: 'series' },
+        data: [350, 240, 144, 300, 130]
+      },
+
     
-   
+      // Repeat for other series...
+    ]
+  });
 
+  useEffect(() => {
+    // Initialize chart
+    myChart.current = echarts.init(chartRef.current, null, {
+      renderer: 'canvas',
+      useDirtyRect: false
+    });
+    myChart.current.setOption(getOption());
 
-  return (
-    <>
-    <div className='bg-white mt-2'>
-      <ReactEcharts option={graph1} />;
-      
-      </div>
-    </>
-  );
-}
+    // Make chart responsive
+    const resizeListener = () => {
+      myChart.current.resize();
+    };
+    window.addEventListener('resize', resizeListener);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+      myChart.current.dispose(); // Dispose chart instance on component unmount
+    };
+  }, []);
+
+  return <div ref={chartRef} style={{ width: '100%', height: '400px' }}></div>;
+};
+
+export default EChartsComponent;
